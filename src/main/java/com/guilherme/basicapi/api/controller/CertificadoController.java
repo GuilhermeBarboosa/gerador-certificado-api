04ef03e9687f3c207c1ad5c7c762cea3475f8385
Guilherme Barbosa <78804519+GuilhermeBarboosa@users.noreply.github.com>
2024-02-02 11:25:07 -0300

    Revert "refactor metodos de arquivos"
    This reverts commit aecf7a43476cc1c4f2adef6caaf4c64695490989.

--- src/main/java/com/guilherme/basicapi/api/controller/CertificadoController.java
+++ src/main/java/com/guilherme/basicapi/api/controller/CertificadoController.java
@@ -1,16 +1,22 @@
 package com.guilherme.basicapi.api.controller;
 
-import com.guilherme.basicapi.api.service.ArquivoService;
 import com.guilherme.basicapi.api.service.CertificadoService;
+import com.guilherme.basicapi.errors.VerifyType;
+import com.guilherme.basicapi.exception.ApiException;
 import com.guilherme.basicapi.model.input.CursoInput;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,31 +27,62 @@ public class CertificadoController {
     private CertificadoService certificadoService;
 
     @Autowired
-    private ArquivoService arquivoService;
+    private VerifyType verifyType;
 
     @PostMapping
-    public void geradorDeCertificado(@RequestParam("urlDados") MultipartFile urlDados,
-                                     @RequestParam("dataInicio") String dataInicio,
-                                     @RequestParam("urlImg") MultipartFile urlImg,
-                                     @RequestParam("urlVerso") MultipartFile urlVerso,
-                                     @RequestParam("qtdHoras") String qtdHoras,
-                                     @RequestParam("nomeCurso") String nomeCurso,
-                                     HttpServletResponse response) throws IOException {
+    public ResponseEntity<?> geradorDeCertificado(@RequestParam("urlDados") MultipartFile urlDados,
+                                       @RequestParam("dataInicio") String dataInicio,
+                                       @RequestParam("urlImg") MultipartFile urlImg,
+                                       @RequestParam("urlVerso") MultipartFile urlVerso,
+                                       @RequestParam("qtdHoras") String qtdHoras,
+                                       @RequestParam("nomeCurso") String nomeCurso) {
 
-        CursoInput cursoInput = new CursoInput(nomeCurso, urlDados, urlImg, urlVerso, qtdHoras);
-        File arquivosGerados = this.certificadoService.gerarCertificado(cursoInput);
-        arquivoService.renderArquivo(response, arquivosGerados);
+        CursoInput cursoInput = new CursoInput( nomeCurso, urlDados, urlImg, urlVerso, qtdHoras);
+//        if (this.verifyType.isTypeFile(cursoInput) != null) {
+//            ApiException exception = new ApiException(this.verifyType.isTypeFile(cursoInput));
+//            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(exception);
+//        }
+
+        return ResponseEntity.ok(this.certificadoService.gerarCertificado(cursoInput));
     }
-
     @PostMapping("/procurar")
     public void procurarCertificado(@RequestBody String cpf, HttpServletResponse response) throws IOException {
-        File arquivo = this.certificadoService.procurarCertificados(cpf);
-        arquivoService.renderArquivo(response, arquivo);
+
+        File arquivo = this.certificadoService.procurarCertificados(cpf); // Seu método para encontrar o arquivo com o CPF
+
+        if (arquivo.isFile()) {
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + arquivo.getName() + "\"");
+
+            try (InputStream inputStream = Files.newInputStream(arquivo.toPath());
+                 OutputStream outputStream = response.getOutputStream()) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            response.setContentType("text/plain");
+            response.getWriter().write("Arquivo não encontrado");
+        }
     }
 
-
-    @GetMapping("/getAll")
-    public void getAll(HttpServletResponse response) throws IOException {
-        arquivoService.renderArquivo(response, certificadoService.getAllCertificados());
+    @PostMapping("/upload")
+    public String uploadCSV(@ModelAttribute CursoInput cursoInput) {
+        System.out.println(cursoInput.toString());
+//        MultipartFile file = certificado.getUrlDados();
+//        System.out.println(file.getContentType());
+//        // Verifique se o arquivo é do tipo text/csv
+//        if (file != null && file.getContentType() != null && file.getContentType().equals("text/csv")) {
+//            System.out.println("Arquivo CSV recebido com sucesso!");
+//            // Aqui você pode salvar, processar ou manipular o arquivo conforme sua lógica
+//            return "Arquivo CSV recebido com sucesso!";
+//        } else {
+//            return "Por favor, envie um arquivo CSV.";
+//        }
+        return "Arquivo CSV recebido com sucesso!";
     }
 }
--- src/main/java/com/guilherme/basicapi/api/service/CertificadoService.java
+++ src/main/java/com/guilherme/basicapi/api/service/CertificadoService.java
@@ -12,9 +12,6 @@ import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveOutputStream;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.compress.utils.IOUtils;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
@@ -23,9 +20,20 @@ import javax.annotation.PostConstruct;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Service
 public class CertificadoService {
@@ -40,99 +48,27 @@ public class CertificadoService {
         caminhoCertificado = filedir + "certificados/";
     }
 
-    public File gerarCertificado(CursoInput cursoInput) {
-        List<UserOutput> usuarios = new ArrayList<>();
-        List<File> fileList = new ArrayList<>();
-        try {
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(cursoInput.getUrlDados().getInputStream()));
-
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-
-            List<CSVRecord> records = csvParser.getRecords();
-
-            if (!records.isEmpty()) {
-                records.remove(0);
-            }
-
-            for (CSVRecord record : records) {
-
-                String column1 = record.get(0);
-
-                String[] values = column1.split(";");
-
-                if (values.length >= 2) {
-
-                    String nome = values[0].trim();
-                    String cpf = values[1].trim();
-
-                    UserOutput userCreated = new UserOutput(nome, cpf, cursoInput);
-                    usuarios.add(userCreated);
-
-                } else {
-                    throw new RuntimeException("Dados inválidos");
-                }
-            }
-
-            csvParser.close();
-
-            for (UserOutput usuario : usuarios) {
-
-                InputStream arquivoJasper = this.getClass().getResourceAsStream("/template-certificado/certificado.jasper");
-
-                String[] palavras = usuario.getNome().toLowerCase().split(" ");
-                StringBuilder resultado = new StringBuilder();
-
-                for (String palavra : palavras) {
-                    if (!palavra.isEmpty()) {
-                        String primeiraLetraMaiuscula = palavra.substring(0, 1).toUpperCase() + palavra.substring(1);
-                        resultado.append(primeiraLetraMaiuscula).append(" ");
-                    }
-                }
-                usuario.setNome(resultado.toString().trim());
-                usuario.setCpf(usuario.getCpf().replaceAll("[^0-9]", ""));
-
-                List<UserOutput> usuarioList = new ArrayList<>();
-                usuarioList.add(usuario);
-
-                JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(usuarioList);
-
-                JasperPrint jasperPrint = JasperFillManager.fillReport(arquivoJasper, null, dataSource);
-
-                byte[] certificado = JasperExportManager.exportReportToPdf(jasperPrint);
-
-                fileList.add(salvarCertificado(usuario, certificado));
-            }
-
-            return getArquivoRar(fileList);
-
-        } catch (IOException | JRException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    private File salvarCertificado(UserOutput userOutput, byte[] certificado) {
+    private void salvarCertificado(UserOutput userOutput, byte[] certificado) {
         try {
             userOutput.setNomeCurso(userOutput.getNomeCurso().replaceAll(" ", "-"));
             String caminhoCompleto = caminhoCertificado + userOutput.getNomeCurso() + "-certificado-" + userOutput.getCpf() + ".pdf";
+
             File file = new File(caminhoCompleto);
 
+            // Certifique-se de que o diretório de destino existe
             if (!file.getParentFile().exists()) {
                 file.getParentFile().mkdirs();
             }
 
+            // Escrever os bytes do relatório no arquivo
             try (FileOutputStream fos = new FileOutputStream(file)) {
                 FileCopyUtils.copy(certificado, fos);
             }
 
-            return file;
         } catch (IOException e) {
             e.printStackTrace();
+            // Trate a exceção conforme necessário
         }
-
-        return null;
     }
 
     public File procurarCertificados(String cpf) {
@@ -147,7 +83,8 @@ public class CertificadoService {
                 if (arquivo.isFile()) {
                     String nomeArquivo = arquivo.getName();
 
-                    Pattern pattern = Pattern.compile("\\d{11}");
+                    // Utilizando expressão regular para encontrar o CPF no nome do arquivo
+                    Pattern pattern = Pattern.compile("\\d{11}"); // Expressão regular para CPF (11 dígitos)
                     Matcher matcher = pattern.matcher(nomeArquivo);
 
                     if (matcher.find()) {
@@ -163,10 +100,7 @@ public class CertificadoService {
             throw new RuntimeException("Diretório não encontrado");
         }
 
-        return getArquivoRar(arquivosEncontrados);
-    }
-
-    private static File getArquivoRar(List<File> arquivosEncontrados) {
+        // Cria um arquivo .rar
         File arquivoRar = new File("certificados_encontrados.rar");
         try (OutputStream arquivoSaida = Files.newOutputStream(arquivoRar.toPath());
              ArchiveOutputStream saidaCompactada = new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.ZIP, arquivoSaida)) {
@@ -182,25 +116,91 @@ public class CertificadoService {
         } catch (IOException | ArchiveException e) {
             e.printStackTrace();
         }
+
         return arquivoRar;
     }
 
-    public File getAllCertificados() {
-        File diretorio = new File(caminhoCertificado);
-        List<File> arquivosEncontrados = new ArrayList<>();
-        File[] arquivos = diretorio.listFiles();
+    public String gerarCertificado(CursoInput cursoInput) {
+        List<UserOutput> usuarios = new ArrayList<>();
 
-        if (arquivos != null) {
-            for (File arquivo : arquivos) {
-                if (arquivo.isFile()) {
-                    arquivosEncontrados.add(arquivo);
+        try {
+            // Lê o arquivo CSV como um InputStream
+            BufferedReader reader = new BufferedReader(new InputStreamReader(cursoInput.getUrlDados().getInputStream()));
+
+            // Cria um CSVParser
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+
+            List<CSVRecord> records = csvParser.getRecords();
+
+            if (!records.isEmpty()) {
+                records.remove(0);
+            }
+
+            // Itera sobre as linhas do arquivo CSV
+            for (CSVRecord record : records) {
+                // Para cada linha, você pode acessar os valores das colunas
+                String column1 = record.get(0); // Obtém o valor da primeira coluna
+
+                String[] values = column1.split(";");
+
+                if (values.length >= 2) {
+                    // Obtém o nome e o CPF separadamente
+                    String nome = values[0].trim(); // Remove espaços em branco extras
+                    String cpf = values[1].trim(); // Remove espaços em branco extras
+                    // Crie seu objeto UserOutput usando nome, cpf e telefone
+
+                    UserOutput userCreated = new UserOutput(nome, cpf, cursoInput);
+                    usuarios.add(userCreated);
+
+                } else {
+                    // Lidar com linhas do CSV que não possuem as duas partes (nome e CPF separados por ';')
+                    //System.out.println("Formato incorreto na linha: " + column1);
                 }
             }
-        } else {
-            throw new RuntimeException("Diretório não encontrado");
+
+            // Fecha o parser
+            csvParser.close();
+
+            for (UserOutput usuario : usuarios) {
+                // Carregar o arquivo Jasper
+                InputStream arquivoJasper = this.getClass().getResourceAsStream("/template-certificado/certificado.jasper");
+
+                //Trocar o nome para primeira letra maiuscula
+                String[] palavras = usuario.getNome().toLowerCase().split(" ");
+                StringBuilder resultado = new StringBuilder();
+
+                for (String palavra : palavras) {
+                    if (!palavra.isEmpty()) {
+                        String primeiraLetraMaiuscula = palavra.substring(0, 1).toUpperCase() + palavra.substring(1);
+                        resultado.append(primeiraLetraMaiuscula).append(" ");
+                    }
+                }
+                usuario.setNome(resultado.toString().trim());
+                usuario.setCpf(usuario.getCpf().replaceAll("[^0-9]", ""));
+                //--------------------------------------
+
+                // Criar uma fonte de dados para o usuario atual
+                List<UserOutput> usuarioList = new ArrayList<>();
+                usuarioList.add(usuario);
+
+                JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(usuarioList);
+
+                // Preencher o relatório com os dados do usuario atual
+                JasperPrint jasperPrint = JasperFillManager.fillReport(arquivoJasper, null, dataSource);
+
+                // Exportar o relatório para um array de bytes (PDF)
+                byte[] certificado = JasperExportManager.exportReportToPdf(jasperPrint);
+
+                // Salvar o certificado
+                salvarCertificado(usuario, certificado);
+
+            }
+
+            return "Arquivo CSV processado com sucesso!";
+
+        } catch (IOException | JRException e) {
+            e.printStackTrace();
+            return "Erro ao processar o arquivo CSV";
         }
-
-        return getArquivoRar(arquivosEncontrados);
     }
-
 }
